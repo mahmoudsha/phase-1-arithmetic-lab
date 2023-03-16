bd99d8cbe4c0a4149380f40f1239f828565f4680
Mahmoud Alshahin <mahmoudshaheen198811@gmail.com>
2023-03-16 23:30:22 +0300

    Done

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 
 // Write your code here
+let num1 = 31
+let num2 = 2
+let multiply = num1 * num2 
+let random  =Math.ceil( Math.random() *10  + 1)
+let num3 = 16
+let num4 = 4
+let mod = num3 / num4 
+let max = Math.max(3,4,20,3,2)
